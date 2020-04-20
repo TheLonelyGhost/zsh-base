24861724b6052b0be935e32e35594802d690a8de
David Alexander <opensource@thelonelyghost.com>
2020-04-19 23:04:12 -0400

    Adds theme to style the PS1

--- init.zsh
+++ init.zsh
@@ -1,5 +1,9 @@
 this="${${(%):-%N}:A:h}"
 
+fpath=("${this}/theme" $fpath)
+autoload -Uz promptinit
+promptinit
+
 for lib in "$this"/libs/*.zsh; do
   source "$lib"
 done
---
+++ libs/theme.zsh
@@ -0,0 +1,3 @@
+
+set promptsubst
+prompt thelonelyghost
