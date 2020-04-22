0edcaf4dd9885f38fc09013a5e0b1b97915c91cc
David Alexander <opensource@thelonelyghost.com>
2020-04-22 10:25:54 -0400

    Handles keychain as gpg-agent and ssh-agent

---
+++ libs/keychain.zsh
@@ -0,0 +1,15 @@
+if [ -z "${SSH_CONNECTION:-}" ] && [ $commands[keychain] ]; then
+  # Except when handling incoming SSH connections (you're on your own, in that case)...
+  keychain --nogui --quiet --agents 'ssh,gpg'
+  export GPG_TTY="$(tty)"
+
+  # this is a fast and an easy way to keep all the terminal tabs in sync with the latest GPG/SSH agent PIDs
+  # if [ -e "${HOME}/.keychain/$(uname -n)-sh" ]; then
+  #   source "${HOME}/.keychain/$(uname -n)-sh"
+  # fi
+  # if [ -e "${HOME}/.keychain/$(uname -n)-sh-gpg" ]; then
+  #   source "${HOME}/.keychain/$(uname -n)-sh-gpg"
+  # fi
+
+  # We'll let ~/.ssh/config add our ssh keys on-the-fly with `AddKeysToAgent` property
+fi
