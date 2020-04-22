723f4066610e786a06ba94b7806117db17689625
David Alexander <opensource@thelonelyghost.com>
2020-04-22 09:35:54 -0400

    Add zsh completion to fpath

--- init.zsh
+++ init.zsh
@@ -1,5 +1,10 @@
 this="${${(%):-%N}:A:h}"
 
+if [ -e /usr/local/share/zsh/site-functions ]; then
+  fpath=(/usr/local/share/zsh/site-functions $fpath)
+fi
+fpath=(~/.zsh/completion $fpath)
+
 for lib in "$this"/libs/*.zsh; do
   source "$lib"
 done
