baee5d8f2d2fe505e97f761cd7b58508b1eda16a
David Alexander <opensource@thelonelyghost.com>
2020-08-10 11:29:03 -0400

    Safer copy-paste using Ctrl+X -> e
    See: https://news.ycombinator.com/item?id=5508341

--- libs/zle_config.zsh
+++ libs/zle_config.zsh
@@ -2,6 +2,11 @@
 
 bindkey -A viins main
 
+# CTRL+X then E will open an editor in which to paste code which will be executed via the command line on exit
+autoload edit-command-line
+zle -N edit-command-line
+bindkey '^Xe' edit-command-line
+
 bindkey '^F' forward-word
 bindkey '^B' backward-word
 
